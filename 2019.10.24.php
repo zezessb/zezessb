df0a4162a826261e75b6898968728a48c11ddf8f
zezessb <56524534+zezessb@users.noreply.github.com>
2019-10-24 15:40:47 +0800

    Rename 2019.10.24 to 2019.10.24.php

---
+++ 2019.10.24.php
@@ -0,0 +1,5 @@
+                    <form action="http://www.baidu.com/baidu" target="_blank">
+                    <input id="kw" type="text" baiduSug="1" size="30" name="word">
+                    <input type="submit" value="百度">
+                    </form>
+                    <script charset="gbk" src="http://www.baidu.com/js/opensug.js"></script>
