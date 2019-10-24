e94724e05fc3b76105c77b99800485e4f9a8b1c5
zezessb <56524534+zezessb@users.noreply.github.com>
2019-10-24 16:33:08 +0800

    Create 2019.10.24(2).php

---
+++ 2019.10.24(2).php
@@ -0,0 +1,11 @@
+  #nav .logo{
+        width: 240px;
+        height: 70px;
+        float: left;
+    }
+     <form action="http://www.baidu.com/baidu" target="_blank">
+                    <input id="kw" type="text" baiduSug="1" size="30" name="word">
+                </form>
+                <!-- 百度搜索框提示 -->
+                    <script charset="gbk" src="http://www.baidu.com/js/opensug.js">
+                    </script>
