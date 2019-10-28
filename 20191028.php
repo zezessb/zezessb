d17eca82d525232e1c23f00b469b65ba46907539
zezessb <56524534+zezessb@users.noreply.github.com>
2019-10-28 10:18:37 +0800

    Create 20191028.php

---
+++ 20191028.php
@@ -0,0 +1,44 @@
+<?php if (!defined('HKCMS_VERSION')) exit(); ?>
+<style type="text/css">
+a{
+    text-decoration: none;
+}
+.container{
+    max-width: 1080px;
+    margin: 0 auto;
+    display: block;
+}
+.fl{
+    float: left;
+}
+.fr{
+    float: right;
+}
+.top-nav{
+    background:#eee;
+}
+.top-nav .item{
+    display: inline-block;
+    padding: 6px 10px;
+    color: #666;
+}
+.top-nav .item:hover{
+    color: #333; 
+}
+   
+</style>
+<header>
+   <div class="top-nav clearfix">
+       <div class="container">
+           <div class="fl">
+               <a class="item" href="#">首页</a href="#">
+           </div>
+           <div class="fr">
+               <a class="item" href="#">我的订单</a herf="#">
+                <a class="item" href="#">收藏夹</a herf="#">
+                <a class="item" href="#">登录</a herf="#">
+                <a class="item" href="#">注册</a herf="#">
+           </div>
+       </div>
+   </div> 
+</header>
